904875e8b8c6c36a8cdcbde2ca9a7a78823b2cc6
Gaganmeet Bahri <gaganmeet@hybrowlabs.com>
2022-01-19 17:34:40 +0530

    feat: starter template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="flex items-center justify-center text-3xl h-screen">
+        Vite Starter Pack by ProDoge
+      </h1>
+    </div>
+  )
+}
+
+export default App
