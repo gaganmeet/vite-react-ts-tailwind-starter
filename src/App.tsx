f3a69e5dec7d27c2365a20b00afe7e5ee7d48249
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2022-06-06 13:09:45 +0530

    fix: upgraded packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1 className="flex items-center justify-center text-3xl h-screen">
+    <div>
+      <h1 className="flex items-center justify-center text-4xl h-screen bg-red-500 text-white">
         Vite Starter Pack by ProDoge
       </h1>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const app = document.getElementById('root')
+if (app) {
+  const root = createRoot(app)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
